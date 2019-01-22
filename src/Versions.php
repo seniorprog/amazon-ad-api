b699b4b28f64e9eef78ba11c168265cb49acfedf
utkir24 <utkir1996@list.ru>
2019-01-23 00:44:48 +0500

    init

---
+++ src/Versions.php
@@ -0,0 +1,10 @@
+<?php
+namespace seniorprog\amazon_ad_api;
+
+class Versions
+{
+    public $versionStrings = array(
+        "apiVersion"         => "v1",
+        "applicationVersion" => "1.2"
+    );
+}
